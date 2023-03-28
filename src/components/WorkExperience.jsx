02c9216b370bb6690200066d69e7f382ae452b2f
Erik Truckner <75428920+ErikTruckner@users.noreply.github.com>
2023-03-28 07:05:28 -0400

    work xp finished

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -11,35 +11,46 @@ import ProjectsCanvas from './canvas/ProjectsCanvas'
 
 const slides = [
   {
-    jobTitle: 'Job 1',
+    jobTitle: 'Job Title',
+    companyName: 'Company Name',
     datesWorked: ' 2020 - 2023 ',
-    jobDescription: 'Put the details of the job and skills required here',
+    jobDescription:
+      'Put the details of the job and skills required here. Example: I worked with React Three Fiber, Framer Motion, and Swiper.js to build amazing looking UI that is reuseable. I used Node and Express to provide functional CRUD operations with a noSQL database.',
   },
   {
-    jobTitle: 'Job 2',
+    jobTitle: 'Job Title',
+    companyName: 'Company Name',
     datesWorked: ' 2020 - 2023',
-    jobDescription: 'Put the details of the job and skills required here',
+    jobDescription:
+      'Put the details of the job and skills required here. Example: I worked with React Three Fiber, Framer Motion, and Swiper.js to build amazing looking UI that is reuseable. I used Node and Express to provide functional CRUD operations with a noSQL database.',
   },
   {
-    jobTitle: 'Job 3',
+    jobTitle: 'Job Title',
+    companyName: 'Company Name',
     datesWorked: ' 2020 - 2023',
-    jobDescription: 'Put the details of the job and skills required here.',
+    jobDescription:
+      'Put the details of the job and skills required here. Example: I worked with React Three Fiber, Framer Motion, and Swiper.js to build amazing looking UI that is reuseable. I used Node and Express to provide functional CRUD operations with a noSQL database.',
   },
   {
-    jobTitle: 'Job 4',
+    jobTitle: 'Job Title',
+    companyName: 'Company Name',
     datesWorked: ' 2020 - 2023',
-    jobDescription: 'Put the details of the job and skills required here',
+    jobDescription:
+      'Put the details of the job and skills required here. Example: I worked with React Three Fiber, Framer Motion, and Swiper.js to build amazing looking UI that is reuseable. I used Node and Express to provide functional CRUD operations with a noSQL database.',
   },
 ]
 
 const WorkExperience = () => {
-  const Card = ({ jobTitle, datesWorked, jobDescription }) => {
+  const Card = ({ jobTitle, companyName, datesWorked, jobDescription }) => {
     return (
-      <div className='border-4 w-full bg-gradient-to-r from-[#069ef67e] to-[#111c6f82] rounded-lg overflow-hidden flex flex-col'>
-        <div className='flex flex-col justify-start items-center   mx-1 my-10'>
+      <div className='border-4 w-[98%] bg-gradient-to-r from-[#069ef67e] to-[#111c6f82] rounded-lg overflow-hidden flex flex-col'>
+        <div className='flex flex-col justify-start items-center  mx-1 my-5'>
           <h2 className='text-white text-lg text-center mb-5'>{jobTitle}</h2>
+          <h3 className='text-white text-lg text-center mb-5'>{companyName}</h3>
           <p className='text-white text-center mb-5'>{datesWorked}</p>
-          <p className='text-white text-center mb-5'>{jobDescription}</p>
+          <p className='text-white text-center mb-5 border-t-2 pt-5'>
+            {jobDescription}
+          </p>
         </div>
       </div>
     )
@@ -57,7 +68,7 @@ const WorkExperience = () => {
           erience
         </h1>
         <p className='mt-5'>Swipe or drag the items</p>
-        <div className='w-full lg:w-1/4 my-10 '>
+        <div className='w-full lg:w-1/4 my-10 ml-2'>
           <Swiper
             grabCursor={true}
             spaceBetween={0}
@@ -81,6 +92,7 @@ const WorkExperience = () => {
               <SwiperSlide key={slide.index}>
                 <Card
                   jobTitle={slide.jobTitle}
+                  companyName={slide.companyName}
                   datesWorked={slide.datesWorked}
                   jobDescription={slide.jobDescription}
                 />
